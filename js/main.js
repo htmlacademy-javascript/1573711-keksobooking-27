e76e54d6fe9874c560903eca38b2b7f333adedcf
Алена <lunymax@gmail.com>
2022-10-01 11:39:44 +0300

    Пишет 2 функции

---
+++ js/main.js
@@ -0,0 +1,31 @@
+// Функция, возвращающая случайное целое число (взято с mdn web docs)
+
+function getRandomIntInclusive(min, max) {
+  if (min < 0 || max < 0 || min > max || min === max) {
+    return NaN;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomIntInclusive(1, 20);
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно (сделала сама)
+
+function getRandomArbitrary(min, max, decimal) {
+  if (min < 0 || max < 0 || min > max || min === max) {
+    return NaN;
+  }
+  // Получить случайное число и уножить на 10 в степени знаков после запятой
+  let number = (Math.random() * (max - min) + min) * Math.pow(10, decimal);
+  // Округлить
+  number = Math.floor(number);
+  // Разделить на количество знаков после запятой
+  number = number / Math.pow(10, decimal);
+
+  return number;
+}
+
+getRandomArbitrary(1, 10, 3);
