b130546197e7eefa8fec25162d2f48dbfa3af614
Алена <lunymax@gmail.com>
2022-10-07 08:53:49 +0300

    Совершенствует функции

--- js/main.js
+++ js/main.js
@@ -7,7 +7,8 @@ function getRandomIntInclusive(min, max) {
 
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  return result;
 }
 
 getRandomIntInclusive(1, 20);
