7f7f0cc037e499a255446e7f77ba69e6e2cf1db7
Алена <lunymax@gmail.com>
2022-10-12 12:01:47 +0300

    Пишет объекты для домашки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 // Функция, возвращающая случайное целое число (взято с mdn web docs)
-
-function getRandomIntInclusive(min, max) {
+function getRandomNumber(min, max) {
   if (min < 0 || max < 0 || min > max || min === max) {
     return NaN;
   }
@@ -11,7 +10,7 @@ function getRandomIntInclusive(min, max) {
   return result;
 }
 
-getRandomIntInclusive(1, 20);
+getRandomNumber(1, 20);
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно (сделала сама)
 
@@ -30,3 +29,121 @@ function getRandomArbitrary(min, max, decimal) {
 }
 
 getRandomArbitrary(1, 10, 3);
+
+//Пишет путь аватарки автора
+function getAvatar() {
+  const someNumber = getRandomNumber(1, 10);
+  let authorAvatar = '';
+
+  if (someNumber < 10) {
+    authorAvatar = `img/avatars/user0${someNumber}.png`;
+    return authorAvatar;
+  }
+
+  authorAvatar = `img/avatars/user${someNumber}.png`;
+  return authorAvatar;
+}
+
+const adTitle = [
+  'Есть свободные места',
+  'Предлагается жилье',
+  'Недалеко от вас'
+];
+
+const adType = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+  'hotel'
+];
+
+const adCheckin = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const adCheckout = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+
+const adFeatures = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+
+const adDescription = [
+  'Переночуйте и уезжайте, ну а что вы хотели за такие деньги',
+  'Неплохое жилье, почти на 3 звезды',
+  'Хорошее жилье по цене-качеству, даже душ в номере есть',
+  'Если у вас много денег, то вам нужно именно это'
+];
+
+const adPhotos = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
+];
+
+// const author =
+// {
+//   avatar: getAvatar()
+// };
+
+// const declarationLocation =
+// {
+//   lat: getRandomArbitrary(35.65000, 35.70000, 4),
+//   lng: getRandomArbitrary(139.70000, 139.80000, 4)
+// };
+
+// const offer =
+// {
+//   title: adTitle[getRandomNumber(0, adTitle.length - 1)],
+//   adress: `${declarationLocation.lat}, ${declarationLocation.lng}`,
+//   price: getRandomNumber(1, 100000),
+//   type: adType[getRandomNumber(0, adType.length - 1)],
+//   rooms: getRandomNumber(1, 10),
+//   guests: getRandomNumber(1, 10),
+//   checkin: adCheckin[getRandomNumber(0, adCheckin.length - 1)],
+//   checkout: adCheckout[getRandomNumber(0, adCheckout.length - 1)],
+//   features: adFeatures[getRandomNumber(0, adFeatures.length - 1)],
+//   description: adDescription[getRandomNumber(0, adDescription.length - 1)],
+//   photos: adPhotos[getRandomNumber(0, adPhotos.length - 1)]
+// };
+
+const createDeclarations = () => ({
+  autor:
+  {
+    avatar: getAvatar()
+  },
+  offer:
+  {
+    title: adTitle[getRandomNumber(0, adTitle.length - 1)],
+    adress: `${declarationLocation.lat}, ${declarationLocation.lng}`,
+    price: getRandomNumber(1, 100000),
+    type: adType[getRandomNumber(0, adType.length - 1)],
+    rooms: getRandomNumber(1, 10),
+    guests: getRandomNumber(1, 10),
+    checkin: adCheckin[getRandomNumber(0, adCheckin.length - 1)],
+    checkout: adCheckout[getRandomNumber(0, adCheckout.length - 1)],
+    features: adFeatures[getRandomNumber(0, adFeatures.length - 1)],
+    description: adDescription[getRandomNumber(0, adDescription.length - 1)],
+    photos: adPhotos[getRandomNumber(0, adPhotos.length - 1)]
+  },
+  location:
+  {
+    lat: getRandomArbitrary(35.65000, 35.70000, 4),
+    lng: getRandomArbitrary(139.70000, 139.80000, 4)
+  }
+});
+
+const declarations = Array.from({length: 10}, createDeclarations);
+
+console.log(declarations);
